57901790664afc2a85a0d558174b55175efc4266
martin0424 <h.newland424@gmail.com>
2022-06-06 20:23:44 -0400

    updated project

--- mecurySedan.js
+++ mecurySedan.js
@@ -1,61 +1,60 @@
 //this includes the vehicle class as a module
-const VehicleModule = require("./vehicleBaseClass")
+const VehicleModule = require("./vehicle.js");
+console.log(VehicleModule);
 
-class Car extends VehicleModule{
-    constructor(make, model, year, color, mileage);
-    
-this.maxPassengers = 5;
+class Car extends VehicleModule.Vehicle {
+  constructor(make, model, year, color, mileage) {
+    super(make, model, year, color, mileage);
+    this.maxPassengers = 5;
     this.passenger = 0;
     this.numberOfWheels = 4;
     this.maxSpeed = 160;
     this.fuel = 10;
     this.scheduleService = false;
-}
+  }
 
-loadPassenger(num);
- {
-    if (this.passenger < this.maxPassengers){
-        if ((num + this.passenger) <= this.maxPassengers) {
-            this.passenger = num;
-            return this.passenger;
-        }
-        else{
-            console.log(this.model + " " + this.make + " do not have enough space to take all passengers.");
-        }
-        else{
-            console.log(this.model + " " + this.make + " is full");
-        }
+  loadPassenger(num) {
+    if (this.passenger < this.maxPassengers) {
+      if (num + this.passenger <= this.maxPassengers) {
+        this.passenger = num;
+        return this.passenger;
+      } else {
+        console.log(
+          this.model +
+            " " +
+            this.make +
+            " do not have enough space to take all passengers."
+        );
+      }
+    } else {
+      console.log(this.model + " " + this.make + " is full");
     }
-}
+  }
 
-start();
- {
-    is (this.fuel > 0);
-     {
-        console.log("engine has started.");
-        return this.started = true
+  start() {
+    if (this.fuel > 0) {
+      console.log("engine has started.");
+      return (this.started = true);
+    } else {
+      console.log("There is no fuel");
+      return (this.started = false);
     }
-    else{
-        console.log("There is no fuel");
-        return this.started = false;
-    }
-}
+  }
 
-scheduleService();
- {
+  scheduleService() {
     if (this.mileage > 30000) {
-        this.scheduleService = true
-        return this.scheduleService;
+      this.scheduleService = true;
+      return this.scheduleService;
     }
+  }
 }
 
 //this shows how to call from this module...
 let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "Black", "43000");
 
+const myCar = new Car("Mecury", "Sedan", "1965", "Black", "43000");
 
-myCar.start()
-myCar.loadPassenger(6)
-myCar.stop()
-myCar.checkService()
+myCar.start();
+myCar.loadPassenger(6);
 
-console.log(v.make)
+console.log(v.make);
